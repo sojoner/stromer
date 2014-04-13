e22b1b7c9db1ef11e673a166ab46af0fd660cd6e
hagen <toennies@retresco.de>
2014-04-13 14:49:03 +0200

    Add a redis producer and consumer.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject stromer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Twitter Redis publish scubscribe example."
+  :url "https://github.com/sojoner/stromer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.taoensso/carmine "2.6.0"]
-                 [twitter-api "0.7.5"]])
+                 [twitter-api "0.7.5"]]
+  :main stromer.producer)
